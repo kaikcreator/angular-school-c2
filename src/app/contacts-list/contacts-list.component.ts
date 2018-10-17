477419f38d2bca3e1ff1da7588390a94718ab37f
-KaiK- <enriqueoriol@gmail.com>
2018-10-17 18:47:33 +0200

    Updated content to match this chapter starter

--- src/app/contacts-list/contacts-list.component.ts
+++ src/app/contacts-list/contacts-list.component.ts
@@ -1,41 +1,22 @@
-import { Component, OnInit, ViewChildren, AfterViewInit, ChangeDetectorRef, QueryList } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Contact } from '../contact.model';
 import { ContactsService } from '../contacts.service';
-import { ContactComponent } from '../contact/contact.component';
 
 @Component({
   selector: 'app-contacts-list',
   templateUrl: './contacts-list.component.html',
   styleUrls: ['./contacts-list.component.scss']
 })
-export class ContactsListComponent implements OnInit, AfterViewInit {
+export class ContactsListComponent implements OnInit {
 
-  @ViewChildren(ContactComponent) contactComponents:QueryList<ContactComponent>;
-  public selectedContact:number = null;
   public contacts:Contact[] = [];
-  constructor(public contactsService:ContactsService, private cdRef:ChangeDetectorRef) { }
+  constructor(public contactsService:ContactsService) { }
 
   ngOnInit() {
     this.contacts = this.contactsService.getContacts();
   }
 
-  ngAfterViewInit(){
-    this.contactComponents.forEach( item => {
-      console.log(item);
-    });
-  }
-
   onContactSelected(id:number){
-    this.selectedContact = id;
-
-    this.contactComponents.forEach(item => {
-      if(item.contact.id === this.selectedContact){
-        item.expanded = !item.expanded;
-      }
-      else{
-        item.expanded = false;
-      }
-    });
   }
 
 }
