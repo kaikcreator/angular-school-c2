836e7336c452cee9ea3f3c35fba48e59ed6d862e
-KaiK- <enriqueoriol@gmail.com>
2018-10-17 17:25:38 +0200

    completed point 8

--- src/app/contacts-list/contacts-list.component.ts
+++ src/app/contacts-list/contacts-list.component.ts
@@ -1,21 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-import { Contact, PhoneType } from '../contact.model';
+import { Component, OnInit, ViewChild, AfterViewInit, ChangeDetectorRef } from '@angular/core';
+import { Contact } from '../contact.model';
 import { ContactsService } from '../contacts.service';
+import { ContactComponent } from '../contact/contact.component';
 
 @Component({
   selector: 'app-contacts-list',
   templateUrl: './contacts-list.component.html',
   styleUrls: ['./contacts-list.component.scss']
 })
-export class ContactsListComponent implements OnInit {
+export class ContactsListComponent implements OnInit, AfterViewInit {
 
+  @ViewChild(ContactComponent) someContact:ContactComponent;
   public selectedContact:number = null;
   public contacts:Contact[] = [];
-  constructor(public contactsService:ContactsService) {
-  }
+  constructor(public contactsService:ContactsService, private cdRef:ChangeDetectorRef) { }
 
   ngOnInit() {
     this.contacts = this.contactsService.getContacts();
+  }
+
+  ngAfterViewInit(){
+    console.log(this.someContact);
+    this.someContact.expanded = true;
+    this.cdRef.detectChanges();
   }
 
   onContactSelected(id:number){
