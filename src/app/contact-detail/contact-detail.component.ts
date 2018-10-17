e3197a68e90bd718dd3b0a777b461a9529522b27
-KaiK- <enriqueoriol@gmail.com>
2018-10-17 18:47:33 +0200

    Updated content to match this chapter starter

---
+++ src/app/contact-detail/contact-detail.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Contact } from '../contact.model';
+
+@Component({
+  selector: 'app-contact-detail',
+  templateUrl: './contact-detail.component.html',
+  styleUrls: ['./contact-detail.component.scss']
+})
+export class ContactDetailComponent implements OnInit {
+
+  public contact: Contact = null;
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
