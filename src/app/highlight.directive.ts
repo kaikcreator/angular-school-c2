ae5b9815d18ba07e0808d9f13838c51a27ad1ac5
-KaiK- <enriqueoriol@gmail.com>
2018-10-23 18:05:15 +0200

    completed point 12

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,10 +1,20 @@
-import { Directive } from '@angular/core';
+import { Directive, Input, ElementRef, OnChanges } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
-export class HighlightDirective {
+export class HighlightDirective implements OnChanges {
+
+  @Input('appHighlight') isHighlighted:boolean = false;
   
-  constructor() { }
+  constructor(private element:ElementRef) {  }
+
+  ngOnChanges(){
+    if(this.isHighlighted){
+      this.element.nativeElement.childNodes[0].style.backgroundColor = '#fdf498'; 
+    }else{
+      this.element.nativeElement.childNodes[0].style.backgroundColor = ''; 
+    }   
+  }
 
 }
