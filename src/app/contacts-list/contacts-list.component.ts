b33e7b1b93623a4d39b0ab3a33ac784aa2f654d8
-KaiK- <enriqueoriol@gmail.com>
2018-11-14 14:12:18 +0100

    completed point 13

--- src/app/contacts-list/contacts-list.component.ts
+++ src/app/contacts-list/contacts-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, HostListener } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Contact } from '../contact.model';
 import { ContactsService } from '../contacts.service';
 
@@ -8,19 +8,8 @@ import { ContactsService } from '../contacts.service';
   styleUrls: ['./contacts-list.component.scss']
 })
 export class ContactsListComponent implements OnInit {
-  public isStickyHeader:boolean = false;
   public contacts:Contact[] = [];
   constructor(public contactsService:ContactsService) { }
-
-  @HostListener('window:scroll', ['$event'])
-  private handleScroll($event:Event){
-    if($event.srcElement.children[0].scrollTop > 20 && !this.isStickyHeader){
-      this.isStickyHeader = true;
-    }
-    else if($event.srcElement.children[0].scrollTop <= 20 && this.isStickyHeader){
-      this.isStickyHeader = false;
-    }
-  }
 
   ngOnInit() {
     this.contacts = this.contactsService.getContacts();
